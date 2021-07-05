f48f16585519203009363ea3eec32cc1f293cd49
Stefan Tudoran <stefantudoran@gmail.com>
2021-07-05 17:39:20 +0300

    Added jackpot animation

--- src/game.tsx
+++ src/game.tsx
@@ -4,37 +4,44 @@ import { reels } from './assets/reel-data/reels'
 import { ReelData } from './models/reel-data'
 import { ReelItemData } from './models/reel-item-data'
 
+import gsap from 'gsap'
+
 @Component({
   tag: 'game-main',
   styleUrl: 'game.scss'
 })
 export class GameMain {
+  private jackpotRef: HTMLDivElement
   private winningItems: ReelItemData[] = []
   private winningCondition: number = 3
   private specialPicId: number = 1
   private animationDuration: number = 3
   private animationSpecialDuraiton: number = 1.5
+  private animationJackpotDuraiton: number = 4
 
   private maxPicId: number = 12
 
   @State() reelsColRef: HTMLSlotReelElement[] = []
   @State() buttonDisabled: boolean = false
   @State() isSpecial: boolean = false
+  @State() isJackpotAnimatioon: boolean = false
 
   private startSpin() {
     this.updateButtonState()
 
-    Promise.all(this.playAnimation()).then((visibleReelItems: ReelItemData[][]) => {
+    Promise.all(this.startSpinAnimation()).then((visibleReelItems: ReelItemData[][]) => {
       this.getWinningReelItems(visibleReelItems.flat())
       this.sendWinningReelItems()
 
       if (this.isSpecial) {
-        Promise.all(this.playAnimation()).then((visibleReelItems: ReelItemData[][]) => {
-          this.getWinningReelItems(visibleReelItems.flat())
-          this.sendWinningReelItems()
-          this.updateButtonState()
+        this.playSpecialAnimatioon().then(() => {
+          Promise.all(this.startSpinAnimation()).then((visibleReelItems: ReelItemData[][]) => {
+            this.getWinningReelItems(visibleReelItems.flat())
+            this.sendWinningReelItems()
+            this.updateButtonState()
 
-          this.isSpecial = false
+            this.isSpecial = false
+          })
         })
       } else {
         this.updateButtonState()
@@ -74,7 +81,7 @@ export class GameMain {
     }
   }
 
-  private playAnimation() {
+  private startSpinAnimation() {
     const visibleItems: Promise<ReelItemData[]>[] = []
 
     for (let i = 0; i < this.reelsColRef.length; i++) {
@@ -86,9 +93,26 @@ export class GameMain {
     return visibleItems
   }
 
+  private playSpecialAnimatioon(): Promise<boolean> {
+    return new Promise(resolve =>
+      gsap.to(this.jackpotRef,
+        {
+          onStart: () => {
+            this.isJackpotAnimatioon = true
+          },
+          onComplete: () => {
+            this.isJackpotAnimatioon = false
+            resolve(true)
+          },
+          duration: this.animationJackpotDuraiton
+        }
+      )
+    )
+  }
+
   private async spinReelItem(reel: HTMLSlotReelElement) {
     const duration = this.isSpecial ? this.animationSpecialDuraiton : this.animationDuration
-    
+
     return reel.spin(duration).then((value: ReelItemData[]) => {
       return value
     })
@@ -99,6 +123,17 @@ export class GameMain {
       <div class="game-view">
         <div class="game-view__slot">
           <div class="game-view__slot__top">
+            <div
+              ref={el => this.jackpotRef = el as HTMLDivElement}
+              class={{
+                "game-view__slot__top__jackpot": true,
+                "special": this.isJackpotAnimatioon
+              }}
+            >
+              <div class="game-view__slot__top__jackpot__text">
+                Jackpot
+              </div>
+            </div>
             <div class="game-view__slot__top__reels">
               {reels?.reels?.map((reelData: ReelData, index: number) =>
                 <slot-reel
